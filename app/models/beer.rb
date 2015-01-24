231eea390ffbfde851f84dc117deca7990378431
wadror <wadror@wadror-VirtualBox.(none)>
2015-01-24 23:07:07 +0200

    ex 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,9 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    all = ratings.collect { |rating| rating.score }
+    all.inject(0.0) {|sum, el| sum + el} / all.size
+  end
 end
